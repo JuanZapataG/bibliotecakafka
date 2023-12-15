3ddb7d5540a17a51c9cd8c164f225c2edfb7fb03
Juan David Zapata Garcia <juadazap@bancolombia.com.co>
2023-12-15 15:09:35 -0500

    Cracion protecto base

---
+++ src/test/java/com/sgb/bibliotecakafka/BibliotecakafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sgb.bibliotecakafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecakafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
